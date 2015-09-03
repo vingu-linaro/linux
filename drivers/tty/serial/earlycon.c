4e1c73b7a13058eb140c3a4a9167781cc2159c7e
Leif Lindholm <leif.lindholm@linaro.org>
2015-09-03 23:01:45 +0100

    of/serial: move earlycon early_param handling to serial
    We have multiple "earlycon" early_param handlers - merge the DT one into
    the main earlycon one in order to reduce the number of implicit
    link-order constraints.
    
    Signed-off-by: Leif Lindholm <leif.lindholm@linaro.org>

--- drivers/tty/serial/earlycon.c
+++ drivers/tty/serial/earlycon.c
@@ -17,6 +17,7 @@
 #include <linux/kernel.h>
 #include <linux/init.h>
 #include <linux/io.h>
+#include <linux/of_fdt.h>
 #include <linux/serial_core.h>
 #include <linux/sizes.h>
 #include <linux/mod_devicetable.h>
@@ -188,7 +189,8 @@ static int __init param_setup_earlycon(char *buf)
 	 * don't generate a warning from parse_early_params() in that case
 	 */
 	if (!buf || !buf[0])
-		return 0;
+		if (IS_ENABLED(CONFIG_OF_FLATTREE))
+			return early_init_dt_scan_chosen_serial();
 
 	err = setup_earlycon(buf);
 	if (err == -ENOENT || err == -EALREADY)
