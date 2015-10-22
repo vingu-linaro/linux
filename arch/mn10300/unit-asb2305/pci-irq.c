706d4cd7aa636f6056ec0461327c5fcbf750eed0
Vality <matt@masarand.com>
2015-10-22 04:53:56 +0100

    mn10300: Defer PCI IRQ assignment to device enable time
    PCI IRQs are being assigned during pcibios_irq_init currently, this causes
    a problem by which hot-plug devices connected after boot will not recieve
    an IRQ, this patch-set fixes this by registering the IRQ assignment
    functions at boot, to then be called later by the device enable code.
    
    Signed-off-by: Matthew Minter <matt@masarand.com>

--- arch/mn10300/unit-asb2305/pci-irq.c
+++ arch/mn10300/unit-asb2305/pci-irq.c
@@ -20,27 +20,11 @@
 #include <asm/smp.h>
 #include "pci-asb2305.h"
 
-void __init pcibios_irq_init(void)
+int pci_map_irq(struct pci_dev *, u8 slot, u8 pin)
 {
-}
+	u8 line;
 
-void __init pcibios_fixup_irqs(void)
-{
-	struct pci_dev *dev = NULL;
-	u8 line, pin;
-
-	for_each_pci_dev(dev) {
-		pci_read_config_byte(dev, PCI_INTERRUPT_PIN, &pin);
-		if (pin) {
-			dev->irq = XIRQ1;
-			pci_write_config_byte(dev, PCI_INTERRUPT_LINE,
-					      dev->irq);
-		}
-		pci_read_config_byte(dev, PCI_INTERRUPT_LINE, &line);
-	}
-}
-
-void pcibios_enable_irq(struct pci_dev *dev)
-{
+	dev->irq = XIRQ1;
 	pci_write_config_byte(dev, PCI_INTERRUPT_LINE, dev->irq);
+	pci_read_config_byte(dev, PCI_INTERRUPT_LINE, &line);
 }
