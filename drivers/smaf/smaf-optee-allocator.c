0f2b3a39770ace4eb8bfea69d237451c96514b5a
Etienne Carriere <etienne.carriere@linaro.org>
2016-09-14 01:20:18 +0200

    smaf: optee allocator using the SMAF TA
    Reuse Pascal's allocator to allocate buffer from the SMAF TA itself.
    This requires few changes in the SMAF TA.
    See github.com/etienne-lms/optee-sdp-fork, branch "memref".
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- drivers/smaf/smaf-optee-allocator.c
+++ drivers/smaf/smaf-optee-allocator.c
@@ -16,93 +16,73 @@
 #include <linux/module.h>
 #include <linux/slab.h>
 #include <linux/smaf-allocator.h>
-#include <uapi/linux/tee.h>
 #include <linux/tee_drv.h>
+#include <uapi/linux/tee.h>
 
-struct smaf_optee_allocator_buffer_info {
+/* Those define are copied from ta_sdp.h */
+#define TA_SDP_UUID { 0xb9aa5f00, 0xd229, 0x11e4, \
+		{ 0x92, 0x5c, 0x00, 0x02, 0xa5, 0xd5, 0xc5, 0x1b} }
+
+#define TA_SDP_ALLOCATE_BUFFER		4
+#define TA_SDP_FREE_BUFFER		5
+
+struct teec_uuid {
+	uint32_t timeLow;
+	uint16_t timeMid;
+	uint16_t timeHiAndVersion;
+	uint8_t clockSeqAndNode[8];
+};
+
+struct tee_secbuf_info {
 	void *dev;
 	size_t size;
 	void *vaddr;
+	size_t kmaped;
 	dma_addr_t paddr;
+	struct sg_table sgt;
 };
 
 static struct {
 	struct tee_context *ctx;
-	__u32 session;
+	uint32_t session;
 } optee_config;
 
-/**
- * smaf_optee_allocator_match - return true if at least one device has been
- * found
- */
-static bool smaf_optee_allocator_match(struct dma_buf *dmabuf)
+static struct sg_table *tee_map_dmabuf(struct dma_buf_attachment *attachment,
+					enum dma_data_direction direction)
 {
-	return !!1;
-}
-
-static void smaf_optee_allocator_release(struct dma_buf *dmabuf)
-{
-	struct smaf_optee_allocator_buffer_info *info = dmabuf->priv;
-	struct tee_ioctl_invoke_arg arg;
-	struct tee_param params[4];
-	DEFINE_DMA_ATTRS(attrs);
-
-	dma_set_attr(DMA_ATTR_WRITE_COMBINE, &attrs);
-
-	memset(&arg, 0, sizeof(arg));
-	arg.func = 1;	/* SMAF_OPTEE_ALLOCATOR_CMD_FREE */
-	arg.session = optee_config.session;
-	arg.num_params = 4;
-
-	/*
-	 * Set the parameters of the command
-	 * - 1st parameter is the physical memory to free (input)
-	 * - 2nd, 3rd and 4th parameters are none
-	 */
-	memset(params, 0, sizeof(params));
-	params[0].attr = TEE_IOCTL_PARAM_ATTR_TYPE_VALUE_INPUT;
-	params[0].u.value.a = info->paddr;
-	params[1].attr = TEE_IOCTL_PARAM_ATTR_TYPE_NONE;
-	params[2].attr = TEE_IOCTL_PARAM_ATTR_TYPE_NONE;
-	params[3].attr = TEE_IOCTL_PARAM_ATTR_TYPE_NONE;
-	tee_client_invoke_func(optee_config.ctx, &arg, params);
-	kfree(info);
-}
-
-static struct sg_table *smaf_optee_allocator_map(struct dma_buf_attachment *attachment,
-				     enum dma_data_direction direction)
-{
-	struct smaf_optee_allocator_buffer_info *info = attachment->dmabuf->priv;
+	struct tee_secbuf_info *info = attachment->dmabuf->priv;
 	struct sg_table *sgt;
-	int ret;
+	int rc;
+
 
 	sgt = kzalloc(sizeof(*sgt), GFP_KERNEL);
 	if (!sgt)
 		return NULL;
 
-	ret = dma_get_sgtable(info->dev, sgt, info->vaddr,
-			      info->paddr, info->size);
-	if (ret < 0)
-		goto out;
+	rc = sg_alloc_table(sgt, 1, GFP_KERNEL);
+	if (rc) {
+		kfree(sgt);
+		return NULL;
+	}
 
+	sg_set_page(sgt->sgl, 0, info->size, 0);
 	sg_dma_address(sgt->sgl) = info->paddr;
-	return sgt;
+	sg_dma_len(sgt->sgl) = info->size;
 
-out:
-	kfree(sgt);
-	return NULL;
+
+	return sgt;
 }
 
-static void smaf_optee_allocator_unmap(struct dma_buf_attachment *attachment,
+static void tee_unmap_dmabuf(struct dma_buf_attachment *attachment,
 				       struct sg_table *sgt,
 				       enum dma_data_direction direction)
 {
 	/* do nothing */
 }
 
-static int smaf_optee_allocator_mmap(struct dma_buf *dmabuf, struct vm_area_struct *vma)
+static int tee_mmap(struct dma_buf *dmabuf, struct vm_area_struct *vma)
 {
-	struct smaf_optee_allocator_buffer_info *info = dmabuf->priv;
+	struct tee_secbuf_info *info = dmabuf->priv;
 	size_t size = vma->vm_end - vma->vm_start;
 
 	vma->vm_flags |= VM_IO | VM_PFNMAP | VM_DONTEXPAND | VM_DONTDUMP;
@@ -111,28 +91,105 @@ static int smaf_optee_allocator_mmap(struct dma_buf *dmabuf, struct vm_area_stru
 			       size, vma->vm_page_prot);
 }
 
-static void *smaf_optee_allocator_vmap(struct dma_buf *dmabuf)
+static void kmap_incr(struct tee_secbuf_info *info)
 {
-	struct smaf_optee_allocator_buffer_info *info = dmabuf->priv;
+	/* mutex_lock, atomic incr/decr */
+	if (!info->vaddr)
+		info->vaddr = ioremap_cache(info->paddr, info->size);
+	if (info->vaddr)
+		info->kmaped++;
+	/* mutex_unlock */
+}
 
+static void kmap_decr(struct tee_secbuf_info *info)
+{
+	/* mutex_lock, atomic incr/decr */
+	if (!info->vaddr)
+		return;
+	if (--info->kmaped)
+		return;
+
+	iounmap(info->vaddr);
+	info->vaddr = NULL;
+	/* mutex_unlock */
+}
+
+static void *tee_vmap(struct dma_buf *dmabuf)
+{
+	struct tee_secbuf_info *info = dmabuf->priv;
+
+	kmap_incr(info);
 	return info->vaddr;
 }
 
-static void *smaf_kmap_atomic(struct dma_buf *dmabuf, unsigned long offset)
+static void tee_vunmap(struct dma_buf *dmabuf, void *vaddr)
 {
-	struct smaf_optee_allocator_buffer_info *info = dmabuf->priv;
+	struct tee_secbuf_info *info = dmabuf->priv;
 
-	return (void *)info->vaddr + offset;
+	kmap_decr(info);
+}
+
+static void *tee_kmap(struct dma_buf *dmabuf, unsigned long off)
+{
+	struct tee_secbuf_info *info = dmabuf->priv;
+
+	kmap_incr(info);
+	return (void *)info->vaddr + off;
+}
+
+static void *tee_kmap_atomic(struct dma_buf *dmabuf, unsigned long off)
+{
+	struct tee_secbuf_info *info = dmabuf->priv;
+
+	if (!info->vaddr)
+		return NULL;
+
+	// atomic_incr(info->kmaped)
+	info->kmaped++;
+	return (void *)info->vaddr + off;
+}
+
+static void  tee_kunmap(struct dma_buf *dmabuf, unsigned long off, void *vaddr)
+{
+	struct tee_secbuf_info *info = dmabuf->priv;
+
+	kmap_decr(info);
+}
+
+static void tee_release(struct dma_buf *dmabuf)
+{
+	struct tee_secbuf_info *info = dmabuf->priv;
+	struct tee_ioctl_invoke_arg arg;
+	struct tee_param param;
+
+	if(!optee_config.session)
+		return;
+
+	memset(&arg, 0, sizeof(arg));
+	arg.func = TA_SDP_FREE_BUFFER;
+	arg.session = optee_config.session;
+	arg.num_params = 1;
+
+	memset(&param, 0, sizeof(param));
+	param.attr = TEE_IOCTL_PARAM_ATTR_TYPE_VALUE_INPUT;
+	param.u.value.a = info->paddr;
+	tee_client_invoke_func(optee_config.ctx, &arg, &param);
+
+	WARN_ON(info->kmaped);
+
+	kfree(info);
 }
 
 static struct dma_buf_ops smaf_optee_allocator_ops = {
-	.map_dma_buf = smaf_optee_allocator_map,
-	.unmap_dma_buf = smaf_optee_allocator_unmap,
-	.mmap = smaf_optee_allocator_mmap,
-	.release = smaf_optee_allocator_release,
-	.kmap_atomic = smaf_kmap_atomic,
-	.kmap = smaf_kmap_atomic,
-	.vmap = smaf_optee_allocator_vmap,
+	.map_dma_buf = tee_map_dmabuf,
+	.unmap_dma_buf = tee_unmap_dmabuf,
+	.mmap = tee_mmap,
+	.release = tee_release,
+	.kmap = tee_kmap,
+	.kmap_atomic = tee_kmap_atomic,
+	.kunmap = tee_kunmap,
+	.vmap = tee_vmap,
+	.vunmap = tee_vunmap,
 };
 
 static int smaf_optee_match(struct tee_ioctl_version_data *data,
@@ -141,132 +198,42 @@ static int smaf_optee_match(struct tee_ioctl_version_data *data,
 	return !!1;
 }
 
-static struct dma_buf *smaf_optee_allocator_allocate(struct dma_buf *dmabuf,
-					 size_t length, unsigned int flags)
+static int tee_connect(void)
 {
-	struct dma_buf_attachment *attach_obj;
-	struct smaf_optee_allocator_buffer_info *info;
-	struct dma_buf *optee_allocator_dmabuf;
-	struct tee_ioctl_invoke_arg arg;
-	struct tee_param params[4];
-	int ret;
-	phys_addr_t paddr;
-	size_t size;
-	phys_addr_t begin;
-	phys_addr_t end;
-
-	DEFINE_DMA_BUF_EXPORT_INFO(export);
-	DEFINE_DMA_ATTRS(attrs);
-	dma_set_attr(DMA_ATTR_WRITE_COMBINE, &attrs);
-
-	info = kzalloc(sizeof(*info), GFP_KERNEL);
-	if (!info)
-		return NULL;
-
-	memset(&arg, 0, sizeof(arg));
-	arg.func = 0;	/* SMAF_OPTEE_ALLOCATOR_CMD_ALLOCATE */
-	arg.session = optee_config.session;
-	arg.num_params = 4;
-
-	/*
-	 * Set the parameters of the command
-	 * - 1st parameter is the size to allocate (input)
-	 * - 2nd parameter is the physical memory (output)
-	 * - 3rd and 4th parameters are none
-	 */
-	memset(params, 0, sizeof(params));
-	params[0].attr = TEE_IOCTL_PARAM_ATTR_TYPE_VALUE_INPUT;
-	params[0].u.value.a = length;
-	params[1].attr = TEE_IOCTL_PARAM_ATTR_TYPE_VALUE_OUTPUT;
-	params[2].attr = TEE_IOCTL_PARAM_ATTR_TYPE_NONE;
-	params[3].attr = TEE_IOCTL_PARAM_ATTR_TYPE_NONE;
-	ret = tee_client_invoke_func(optee_config.ctx,
-			&arg,
-			params);
-
-	if (ret)
-		goto error;
-	if (arg.ret) {
-		ret = -EINVAL;
-		goto error;
-	}
-
-	info->paddr = params[1].u.value.a;
-	info->size = length;
-	if (!info->paddr) {
-		ret = -ENOMEM;
-		goto error;
-	}
-
-	begin = roundup(info->paddr, PAGE_SIZE);
-	end = rounddown(info->paddr + info->size, PAGE_SIZE);
-	paddr = begin;
-	size = end - begin;
-
-	info->vaddr = ioremap_cache(paddr, size);
-	info->dev = optee_config.ctx;
-
-	export.ops = &smaf_optee_allocator_ops;
-	export.size = info->size;
-	export.flags = flags;
-	export.priv = info;
-
-	optee_allocator_dmabuf = dma_buf_export(&export);
-	if (IS_ERR(optee_allocator_dmabuf))
-		goto error;
-
-	list_for_each_entry(attach_obj, &dmabuf->attachments, node) {
-		dma_buf_attach(optee_allocator_dmabuf, attach_obj->dev);
-	}
-
-	return optee_allocator_dmabuf;
-
-error:
-	kfree(info);
-	return NULL;
-}
-
-static struct smaf_allocator smaf_optee_allocator = {
-	.match = smaf_optee_allocator_match,
-	.allocate = smaf_optee_allocator_allocate,
-	.name = "smaf-optee-allocator",
-	.ranking = 1,
-};
-
-static int __init smaf_optee_allocator_init(void)
-{
-	int ret;
+	int rc;
 	struct tee_ioctl_version_data vers = {
 		.impl_id = TEE_OPTEE_CAP_TZ,
 		.impl_caps = TEE_IMPL_ID_OPTEE,
 		.gen_caps = TEE_GEN_CAP_GP,
 	};
-	const __u8 uuid[TEE_IOCTL_UUID_LEN] = {
-		0x47, 0x07, 0x2c, 0x63, 0x30, 0xcf, 0x3a, 0x44,
-		0x8f, 0x16, 0x0f, 0xcb, 0x01, 0xf1, 0xf5, 0x9a};
+	const struct teec_uuid uuid = TA_SDP_UUID;
 	struct tee_ioctl_open_session_arg arg;
+
+	pr_err("request connect tee\n");
 
 	optee_config.ctx = tee_client_open_context(NULL,
 						   smaf_optee_match,
 						   NULL, &vers);
+	pr_err("request connect tee done\n");
 	if (IS_ERR(optee_config.ctx))
 		return PTR_ERR(optee_config.ctx);
 
 	memset(&arg, 0, sizeof(arg));
-	memcpy(arg.uuid, uuid, TEE_IOCTL_UUID_LEN);
-	ret = tee_client_open_session(optee_config.ctx,
+	memcpy(arg.uuid, &uuid, TEE_IOCTL_UUID_LEN);
+	rc = tee_client_open_session(optee_config.ctx,
 			&arg, NULL);
-	if (ret)
+	if (rc)
 		goto error;
 	if (arg.ret) {
-		ret = -EINVAL;
+		rc = -EINVAL;
 		goto error;
 	}
 	optee_config.session = arg.session;
+	pr_debug("optee alloc: regsiter smaf\n");
 
-	INIT_LIST_HEAD(&smaf_optee_allocator.list_node);
-	return smaf_register_allocator(&smaf_optee_allocator);
-
+	pr_err("request connected to tee\n");
+	
+	return 0;
 error:
 	if (optee_config.session) {
 		tee_client_close_session(optee_config.ctx,
@@ -278,11 +245,99 @@ error:
 		tee_client_close_context(optee_config.ctx);
 		optee_config.ctx = 0;
 	}
-	return ret;
+	return rc;
 }
-module_init(smaf_optee_allocator_init);
+/* hack */
+struct device *get_smaf_dev(void);
 
-static void __exit smaf_optee_allocator_deinit(void)
+static struct dma_buf *tee_alloc(struct dma_buf *dmabuf, size_t length)
+{
+	struct dma_buf_attachment *attach_obj;
+	struct tee_secbuf_info *info;
+	struct dma_buf *tee_dmabuf;
+	struct tee_ioctl_invoke_arg arg;
+	struct tee_param params;
+	int rc;
+	DEFINE_DMA_BUF_EXPORT_INFO(export);
+	DEFINE_DMA_ATTRS(attrs);
+
+	dma_set_attr(DMA_ATTR_WRITE_COMBINE, &attrs);
+
+	if (!optee_config.session && tee_connect())
+		return ERR_PTR(-EIO);
+
+	memset(&arg, 0, sizeof(arg));
+	arg.func = TA_SDP_ALLOCATE_BUFFER;
+	arg.session = optee_config.session;
+	arg.num_params = 1;
+
+	memset(&params, 0, sizeof(params));
+	params.attr = TEE_IOCTL_PARAM_ATTR_TYPE_VALUE_INOUT;
+	params.u.value.b = length;
+
+	rc = tee_client_invoke_func(optee_config.ctx, &arg, &params);
+	if (rc)
+		return ERR_PTR(rc);
+	if (arg.ret)
+		return ERR_PTR(-EINVAL);
+
+	info = kzalloc(sizeof(*info), GFP_KERNEL);
+	if (!info)
+		return ERR_PTR(-ENOMEM);
+
+	info->dev = get_smaf_dev();
+
+	info->paddr = params.u.value.a;
+	info->size = params.u.value.b;
+
+	if ((info->paddr & (PAGE_SIZE - 1)) ||
+	   (info->size & (PAGE_SIZE - 1))) {
+		dev_err(info->dev, "unexpected unaligne buffer %p /0x%x\n",
+			(void *)info->paddr, info->size);
+		rc = -EINVAL;
+		goto error;
+	}
+
+	export.ops = &smaf_optee_allocator_ops;
+	export.size = info->size;
+	export.priv = info;
+
+	tee_dmabuf = dma_buf_export(&export);
+	if (IS_ERR(tee_dmabuf))
+		goto error;
+
+	list_for_each_entry(attach_obj, &dmabuf->attachments, node) {
+		dma_buf_attach(tee_dmabuf, attach_obj->dev);
+	}
+
+	return tee_dmabuf;
+
+error:
+	kfree(info);
+	return ERR_PTR(rc);
+}
+
+static bool tee_allocator_match(struct dma_buf *dmabuf)
+{
+	return !!1;
+}
+
+static struct smaf_allocator smaf_optee_allocator = {
+	.match = tee_allocator_match,
+	.allocate = tee_alloc,
+	.name = "smaf-optee",
+	.ranking = 1,
+};
+
+static int __init tee_allocator_init(void)
+{
+	INIT_LIST_HEAD(&smaf_optee_allocator.list_node);
+
+	return smaf_register_allocator(&smaf_optee_allocator);
+}
+module_init(tee_allocator_init);
+
+static void __exit tee_allocator_deinit(void)
 {
 	if (optee_config.session) {
 		tee_client_close_session(optee_config.ctx,
@@ -296,9 +351,9 @@ static void __exit smaf_optee_allocator_deinit(void)
 	}
 	smaf_unregister_allocator(&smaf_optee_allocator);
 }
-module_exit(smaf_optee_allocator_deinit);
+module_exit(tee_allocator_deinit);
 
-MODULE_DESCRIPTION("SMAF OPTEE Allocator module");
+MODULE_DESCRIPTION("SMAF TEE Allocator module");
 MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("Pascal Brand <pascal.brand@linaro.org>");
 
