930de15daee5de7d65c89ad03ef1aea1a2979e24
Etienne Carriere <etienne.carriere@linaro.org>
2019-09-09 11:21:50 +0200

    [PATCH V2 tst] test probe a subsys, not core

--- drivers/mailbox/arm-smc-mailbox.c
+++ drivers/mailbox/arm-smc-mailbox.c
@@ -72,6 +72,8 @@ static irqreturn_t chan_irq_handler(int irq, void *data)
 {
 	struct mbox_chan *chan = data;
 
+//pr_info("smc-mailbox: received data by irq\n");
+
 	mbox_chan_received_data(chan, NULL);
 
 	return IRQ_HANDLED;
@@ -198,7 +200,15 @@ static struct platform_driver arm_smc_mbox_driver = {
 	.probe		= arm_smc_mbox_probe,
 	.remove		= arm_smc_mbox_remove,
 };
-module_platform_driver(arm_smc_mbox_driver);
+//module_platform_driver(arm_smc_mbox_driver);
+
+static int __init arm_smc_mbox_driver_init(void)
+{
+	pr_err("%s----------------------------\n", __func__);
+	return platform_driver_register(&arm_smc_mbox_driver);
+}
+//core_initcall(arm_smc_mbox_driver_init);
+subsys_initcall(arm_smc_mbox_driver_init);
 
 MODULE_AUTHOR("Andre Przywara <andre.przywara@arm.com>");
 MODULE_DESCRIPTION("Generic ARM smc mailbox driver");
