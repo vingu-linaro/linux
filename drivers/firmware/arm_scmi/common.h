aef50b3b2f82eecaef4afcb4597c1416979bf369
Etienne Carriere <etienne.carriere@linaro.org>
2020-03-28 13:52:59 +0100

    firmware: arm_scmi: factorize root node  test
    Remove transport callback handler chan_available that can be factorize
    for all transports. Indeed SCMI child nodes defining each expected
    SCMI protocol define expect protocol ID through FDT property "reg"
    whereas SCMI device root node does not define such "reg" property.
    
    This change replaces need for transport specific chan_available
    handler with a common test on SCMI root node to determine if currently
    probed device is the SCMI device which shall define the channel to be
    used whereas SCMI child node only define target protocols.
    
    This change updates mailbox transport driver accordingly.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- drivers/firmware/arm_scmi/common.h
+++ drivers/firmware/arm_scmi/common.h
@@ -174,7 +174,6 @@ struct scmi_chan_info {
 /**
  * struct scmi_transport_ops - Structure representing a SCMI transport ops
  *
- * @chan_available: Callback to check if channel is available or not
  * @chan_setup: Callback to allocate and setup a channel
  * @chan_free: Callback to free a channel
  * @send_message: Callback to send a message
@@ -183,7 +182,6 @@ struct scmi_chan_info {
  * @poll_done: Callback to poll transfer status
  */
 struct scmi_transport_ops {
-	bool (*chan_available)(struct device *dev, int idx);
 	int (*chan_setup)(struct scmi_chan_info *cinfo, struct device *dev,
 			  bool tx);
 	int (*chan_free)(int id, void *p, void *data);
