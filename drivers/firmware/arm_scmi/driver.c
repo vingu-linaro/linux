41722d2398b8709ea2a655d02e77615510e28aa6
Etienne Carriere <etienne.carriere@linaro.org>
2020-03-28 14:18:53 +0100

    firmware: arm_scmi: new optee transport layer
    TODO: remove shmem from the bindings. Message buffer should be
          provided through a memref parameter to OP-TEE PTA invocation.
    
    Add a new transport channel to the SCMI firmware interface driver for
    SCMI message exchange based on OP-TEE transport channel that leverage
    OP-TEE secure threaded context for processing of SCMI messages.
    
    The current proposal uses a statically defined physical memory area
    to be used as shared memory SCMI endpoints. The location is extracted
    from the FDT upon property "shmem".
    
    Entry in OP-TEE threaded context is realized by invoking a service
    PTA (Pseudo TA) in OP-TEE OS. Each invocation carries a agent
    numerical identifier for which a message is pending in the shared
    memory. The OP-TEE service provides means to get the agent identifier
    value for a given shared memory location.
    
    OPTEE transport driver depends on CONFIG_OPTEE and probes from SCMI
    compatible identifier "arm,scmi-optee".
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>

--- drivers/firmware/arm_scmi/driver.c
+++ drivers/firmware/arm_scmi/driver.c
@@ -913,6 +913,9 @@ static const struct of_device_id scmi_of_match[] = {
 #ifdef CONFIG_HAVE_ARM_SMCCC_DISCOVERY
 	{ .compatible = "arm,scmi-smc", .data = &scmi_smc_desc},
 #endif
+#ifdef CONFIG_OPTEE
+	{ .compatible = "linaro,scmi-optee", .data = &scmi_optee_desc },
+#endif
 	{ /* Sentinel */ },
 };
 
